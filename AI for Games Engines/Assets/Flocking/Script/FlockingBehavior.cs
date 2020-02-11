288fc98a4e186d4c52c434abfa89ca36c715c636
natxtw <36550708+natxtw@users.noreply.github.com>
2020-02-11 02:58:02 +0000

    First Version
    Agents Spawn and core started

---
+++ AI for Games Engines/Assets/Flocking/Script/FlockingBehavior.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class FlockingBehavior : ScriptableObject
+{
+    //Using an abstract class because i don't want to instansiate any behaviour in this script
+
+    public abstract Vector2 MovementCalculation(FlockingAgent agent, List<Transform> Neighbours, Flocking Flock);
+}
