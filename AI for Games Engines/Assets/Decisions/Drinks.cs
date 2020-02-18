df062ac52bf8beb969b703298e0f1577ae64952c
Callum-Robinson5 <47246433+Callum-Robinson5@users.noreply.github.com>
2020-02-18 12:16:25 +0000

    commit
    commit

---
+++ AI for Games Engines/Assets/Decisions/Drinks.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drinks : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "Peep")
+        {
+            print("drink");
+        }
+    }
+}
