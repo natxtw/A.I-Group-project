c4f4c59e2cb0796432d50fa192deac6c80cfe8cb
apPAULling <60662385+apPAULling@users.noreply.github.com>
2020-03-10 12:14:19 +0000

    Add files via upload

---
+++ Pathfinding/Node.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Nodes
+{
+    public class Map // Node functionality for pathfinding
+    {
+        public static Map Randomize(int NodeCount, int Branching, int Seed, bool RandomWeights)
+        {
+            var rnd = new Random(Seed);
+            var map = new Map();
+
+            for(int i = 0; i < NodeCount; i++)
+            {
+                var newNode = Node.GetRandom(rnd, i.ToString());
+                if (!newNode.ToCloseToAny(map.Nodes))
+                    map.Nodes.Add(newNode);
+            }
+
+            foreach (var node in map.Nodes)
+                node.ConnectClosestNodes(map.Nodes, Branching, rnd, RandomWeights);
+            map.EndNode = map.Nodes[rnd.Next(map.Nodes.Count - 1)];
+            map.StartNode = map.Nodes[rnd.Next(map.Nodes.Count - 1)];
+
+            return map;
+        }
+
+        public List<Node> Nodes { get; set; } = new List<Node>(); // World map
+        public Node StartNode { get; set;  } // Current location
+        public Node EndNode { get; set;  } // Destination from decision making
+        public List<Node> ShortestPath { get; set; } = new List<Node>();
+    }
+
+    public class Node // Map tile
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Point Point { get; set; }
+        public List<Edge> Connections { get; set; }
+        public double? MinCostToStart { get; set; }
+        public Node NearestToStart { get; set; }
+        public bool Visited { get; set; }
+        public double StraightLineDistanceToEnd { get; set; }
+
+        internal static Node GetRandom(Random rnd, string name)
+        {
+            return new Node
+            {
+                Point = new Point
+                {
+                    X = rnd.NextDouble(),
+                    Y = rnd.NextDouble()
+                },
+                Id = Guid.NewGuid(),
+                Name = name
+            };
+        }
+
+        internal void ConnectClosestNodes(List<Node> nodes, int Branching, Random rnd, bool RandomWeight)
+        {
+            var connections = new List<Edge>();
+            foreach(var node in nodes)
+            {
+                if (node.Id == this.Id)
+                    continue;
+                var Distance = Math.Sqrt(Math.Pow(Point.X - node.Point.X, 2) + Math.Pow(Point.Y - node.Point.Y, 2));
+                connections.Add(new Edge
+                {
+                    ConnectedNode = node,
+                    Length = Distance,
+                    Cost = RandomWeight ? rnd.NextDouble() : Distance,
+                });
+            }
+            connections = connections.OrderBy(x => x.Length).ToList();
+            var count = 0;
+            foreach (var cnn in connections)
+            {
+                if (!Connections.Any(c => c.ConnectedNode == cnn.ConnectedNode))
+                    Connections.Add(cnn);
+                count++;
+
+                if(!cnn.ConnectedNode.Connections.Any(cc => cc.ConnectedNode == this))
+                {
+                    var backConnection = new Edge { ConnectedNode = this, Length = cnn.Length };
+                    cnn.ConnectedNode.Connections.Add(backConnection);
+                }
+                if (count == Branching)
+                    return;
+            }
+        }
+
+        public double StraightLineDistanceTo(Node end)
+        {
+            return Math.Sqrt(Math.Pow(Point.X - end.Point.X, 2) + Math.Pow(Point.Y - end.Point.Y, 2));
+        }
+
+        internal bool ToCloseToAny(List<Node> nodes)
+        {
+            foreach (var node in nodes)
+            {
+                var d = Math.Sqrt(Math.Pow(Point.X - node.Point.X, 2) + Math.Pow(Point.Y - node.Point.Y, 2));
+                if (d < 0.01)
+                    return true;
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    public class Edge
+    {
+            public double Length { get; set; }
+            public double Cost { get; set; } // 1 for path, 100 for grass to ensure guests move on the path
+            public Node ConnectedNode { get; set; }
+        public override string ToString()
+        {
+            return "-> " + ConnectedNode.ToString();
+        }
+    }
+
+    public class Point
+    {
+            public double X { get; set; }
+            public double Y { get; set; }
+    }
+}
---
+++ Pathfinding/Pathfinding.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+using Assets.Nodes;
+
+namespace Assets.Pathfinding
+{
+    public class Pathfinding // Actual algorithm for pathing
+    {
+        public event EventHandler Updated;
+        private void OnUpdated()
+        {
+            Updated?.Invoke(null, EventArgs.Empty);
+        }
+        public Map Map { get; set; }
+        public Node Start { get; set; }
+        public Node End { get; set; }
+        public int NodeVisits { get; private set; }
+        public double ShortestPathLength { get; set; }
+        public double ShortestPathCost { get; private set; }
+
+        public Pathfinding(Map map)
+        {
+            Map = map;
+            End = map.EndNode;
+            Start = map.StartNode;
+        }
+
+        // Djikstra search as contingency 
+
+        /*
+        public List<Node> GetShortestPathDijkstra()
+        {
+            DijkstraSearch();
+            var shortestPath = new List<Node>();
+            shortestPath.Add(End);
+            BuildShortestPath(shortestPath, End);
+            shortestPath.Reverse();
+            return shortestPath;
+        }
+       
+        private void DijkstraSearch()
+        {
+            NodeVisits = 0;
+            Start.MinCostToStart = 0;
+            var prioQueue = new List<Node>();
+            prioQueue.Add(Start);
+            do
+            {
+                NodeVisits++;
+                prioQueue = prioQueue.OrderBy(x => x.MinCostToStart.Value).ToList();
+                var node = prioQueue.First();
+                prioQueue.Remove(node);
+                foreach (var cnn in node.Connections.OrderBy(x => x.Cost))
+                {
+                    var childNode = cnn.ConnectedNode;
+                    if (childNode.Visited)
+                        continue;
+                    if (childNode.MinCostToStart == null || node.MinCostToStart + cnn.Cost < childNode.MinCostToStart)
+                    {
+                        childNode.MinCostToStart = node.MinCostToStart + cnn.Cost;
+                        childNode.NearestToStart = node;
+                        if (!prioQueue.Contains(childNode))
+                            prioQueue.Add(childNode);
+                    }
+                }
+                node.Visited = true;
+                if (node == End)
+                    return;
+            } while (prioQueue.Any());
+        }
+        */
+
+        private void BuildShortestPath(List<Node> list, Node node)
+        {
+            if (node.NearestToStart == null)
+                return;
+            list.Add(node.NearestToStart);
+            ShortestPathLength += node.Connections.Single(x => x.ConnectedNode == node.NearestToStart).Length;
+            ShortestPathCost += node.Connections.Single(x => x.ConnectedNode == node.NearestToStart).Cost;
+            BuildShortestPath(list, node.NearestToStart);
+        }
+
+        public
+            List<Node> AStarShortPath()
+        {
+            foreach (var node in Map.Nodes)
+                node.StraightLineDistanceToEnd = node.StraightLineDistanceTo(End);
+            AStarSearch();
+            var ShortestPath = new List<Node>();
+            ShortestPath.Add(End);
+            BuildShortestPath(ShortestPath, End);
+            ShortestPath.Reverse();
+            return ShortestPath;
+        }
+
+        private
+        void AStarSearch()
+        {
+            NodeVisits = 0;
+            Start.MinCostToStart = 0;
+            var PriorityQueue = new List<Node>();
+            PriorityQueue.Add(Start);
+            do
+            {
+                PriorityQueue = PriorityQueue.OrderBy(x => x.MinCostToStart + x.StraightLineDistanceToEnd).ToList();
+                var SelectedNode = PriorityQueue.First();
+                PriorityQueue.Remove(SelectedNode);
+                NodeVisits++;
+                foreach (var cnn in SelectedNode.Connections.OrderBy(x => x.Cost))
+                {
+                    var ChildNode = cnn.ConnectedNode;
+                    if (ChildNode.Visited)
+                        continue;
+                    if (ChildNode.MinCostToStart == null || SelectedNode.MinCostToStart + cnn.Cost < ChildNode.MinCostToStart)
+                    {
+                        ChildNode.MinCostToStart = SelectedNode.MinCostToStart + cnn.Cost;
+                        ChildNode.NearestToStart = SelectedNode;
+                        if (!PriorityQueue.Contains(ChildNode))
+                            PriorityQueue.Add(ChildNode);
+                    }
+                }
+                SelectedNode.Visited = true;
+                if (SelectedNode == End)
+                    return;
+            } while (PriorityQueue.Any());
+        }
+    }
+}
